df27df54bc9515343c272631cf12ca4992414ca7
apolokastro <angelbaezc117@gmail.com>
2022-10-05 13:21:20 -0500

    First commit

---
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -0,0 +1,14 @@
+package cucumberOptions;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "src/test/java/features",
+				 glue = "stepDefinitions", 
+				 monochrome = true, 
+				 dryRun = true, 
+				 plugin = "summary")
+
+public class TestNGTestRunner extends AbstractTestNGCucumberTests{
+
+}
---
+++ src/test/java/stepDefinitions/GreenKart.java
@@ -0,0 +1,24 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class GreenKart {
+	
+	@Given("I want to write a step with precondition")
+	public void i_want_to_write_a_step_with_precondition() {
+	    System.out.println("one");
+	}
+	@And("some other precondition")
+	public void some_other_precondition() {
+		System.out.println("two");
+	}
+	@When("I complete action")
+	public void i_complete_action() {
+		System.out.println("three");
+	}
+	@Then("I validate the outcomes")
+	public void i_validate_the_outcomes() {
+		System.out.println("four");
+	}
+	
+}
