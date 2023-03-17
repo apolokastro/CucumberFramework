e511c22ef1c10fd17bdb4c75b4d7c74ff2368382
apolokastro <angelbaezc117@gmail.com>
2023-03-17 15:52:30 -0600

    Reports

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/features",
-				 glue = "stepDefinitions", monochrome = true, plugin = {"pretty", "html:target/cucumber.html"})
+				 glue = "stepDefinitions", monochrome = true, plugin = {"pretty", "html:target/cucumber.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 	
